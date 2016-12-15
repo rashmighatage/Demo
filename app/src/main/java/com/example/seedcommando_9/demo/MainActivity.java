0ff9f6e3c020d87bc900bfb94821c629a0078628
seedcommando_9 <rash.ghatage58@gmail.com>
2016-12-15 16:32:00 +0530

    Toast hello

---
+++ app/src/main/java/com/example/seedcommando_9/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.seedcommando_9.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"Hello",Toast.LENGTH_LONG).toString();
+    }
+}
